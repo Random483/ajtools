1889a6dde2443585a879c68aa99d6ab906e3cae5
Andrew Ross <andrew2.ross@education.gov.uk>
2023-04-04 16:54:12 +0100

    identical

--- R/identical_vectorised.r
+++ R/identical_vectorised.r
@@ -2,6 +2,7 @@
 #'
 #' this function checks to see if two columns are equal, row by row.
 #' It handles cases where NA exists too
+#' @export
 aj_identical_vectorised <- function(x, y) {
     a <- (x != y | (is.na(x) | is.na(y))) & !(is.na(x) & is.na(y))
     return(a)
